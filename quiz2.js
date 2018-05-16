c68088449e005a8f28dea33ca7e08ef5bcef0177
Nic B <n1scby@yahoo.com>
2018-05-16 14:56:52 -0400

    component and sub component

---
+++ quiz2.js
@@ -0,0 +1,110 @@
+"use strict";
+
+var quizTitle = 'Quiz 1';
+var questionNumber = 0;
+
+var questions = [
+    {
+    questionId: 1,
+    question: 'What is my favorite soccer team in MLS?',
+     type: 'Multi',
+     answers: [{answer: 'NY FC',
+                id: 1},
+                {answer: 'DC United',
+                id: 2},
+                {answer: 'Atlanta United FC',
+                id: 3},
+                {answer: 'Minnesota United',
+                id: 4}],
+     answerId: 3
+    },
+     {
+        questionId: 2,
+        question: 'Which is better on French Fries?',
+         type: 'Multi',
+         answers: [{answer: 'Ketchup',
+                    id: 1},
+                    {answer: 'Mustard',
+                    id: 2},
+                    {answer: 'Mayo',
+                    id: 3},
+                    {answer: 'Pickle Juice',
+                    id: 4}],
+         answerId: 1
+
+     },
+
+     {
+        questionId: 3,
+        question: 'What goes best with Mexican Food?',
+         type: 'Multi',
+         answers: [{answer: 'Ketchup',
+                    id: 1},
+                    {answer: 'Margarita',
+                    id: 2},
+                    {answer: 'Orange Juice',
+                    id: 3},
+                    {answer: 'Pickle Juice',
+                    id: 4}],
+         answerId: 2
+
+     }
+    ];
+
+
+
+
+
+    Vue.component('question', {
+        props: ['question',  'answerlist'],
+        template: `
+                <div>
+                    <div>Question: {{question}} </div>
+                    <div>
+                        <ul>
+                        <answer v-for="answer in answerlist" v-bind:key="answer.Id" v-bind:next="answer.answer"></answer>
+                        </ul>
+                    </div>
+                </div>
+        
+        `,
+        data() {
+            return{
+                questions: [],
+                answers: []
+            }
+        }
+    })
+
+
+    Vue.component('answer',{
+        props: ['next'],
+        template: `
+                    <li>{{ next }}</li>
+        
+        `,
+        data() {
+            return{
+                answers: []
+            }
+        }
+
+
+    })
+
+    new Vue({
+        el: '#quiz',
+        data: { 
+            question: questions[questionNumber].question,
+            answers: questions[questionNumber].answers        
+        },
+        methods: {
+            nextQuestion(){
+                if (questionNumber <= 1) {
+                questionNumber++;
+                this.question = questions[questionNumber].question;
+                this.answers = questions[questionNumber].answers;
+                }
+            }
+        }
+    })
