46041cf2abc1b1beb81694438d0011f81ed5c5f4
Nic B <n1scby@yahoo.com>
2018-05-17 19:16:15 -0400

    quiz component - quiz 3 started quiz 4 with input

---
+++ quiz4.js
@@ -0,0 +1,137 @@
+"use strict";
+
+var quizTitle = 'Quiz 1';
+var questionNumber = 0;
+
+var questions = [
+    {
+    questionId: 1,
+    question: 'What is my favorite soccer team in MLS?',
+     type: 'Multi',
+     answers: [{answer: 'NY FC',
+                id: 1},
+                {answer: 'DC United',
+                id: 2},
+                {answer: 'Atlanta United FC',
+                id: 3},
+                {answer: 'Minnesota United',
+                id: 4}],
+     answerId: 3
+    },
+     {
+        questionId: 2,
+        question: 'Which is better on French Fries?',
+         type: 'Multi',
+         answers: [{answer: 'Ketchup',
+                    id: 1},
+                    {answer: 'Mustard',
+                    id: 2},
+                    {answer: 'Mayo',
+                    id: 3},
+                    {answer: 'Pickle Juice',
+                    id: 4}],
+         answerId: 1
+
+     },
+
+     {
+        questionId: 3,
+        question: 'What goes best with Mexican Food?',
+         type: 'Multi',
+         answers: [{answer: 'Ketchup',
+                    id: 1},
+                    {answer: 'Margarita',
+                    id: 2},
+                    {answer: 'Orange Juice',
+                    id: 3},
+                    {answer: 'Pickle Juice',
+                    id: 4}],
+         answerId: 2
+
+     }
+    ];
+
+
+
+    Vue.component('quiz', {
+        props: [],
+        template: `
+                <div>
+                    <h2>{{ title }}</h2>
+                    <div>
+                        <question v-bind:thisquestion="question"> </question>
+                    </div>
+                    <div>
+                    <ul>
+                    <answer v-for="answer in answers" v-bind:key="answer.Id" v-bind:next="answer.answer" v-bind:answerid="answer.Id"></answer>
+                    </ul>
+                </div>
+                    <button v-on:click="nextQuestion()">next Question</button>
+                </div>
+                `,
+                data() {
+                    return{
+                        title: "Quiz 3",
+                        question: questions[questionNumber].question,
+                        answers: questions[questionNumber].answers   
+                    }
+                },
+                methods:  {
+                    nextQuestion(){
+                        if (questionNumber <= 1) {
+                        questionNumber++;
+                        this.question = questions[questionNumber].question;
+                        this.answers = questions[questionNumber].answers;
+                        }
+                    }
+                }
+    })
+
+
+    Vue.component('question', {
+        props: ['thisquestion'],
+        template: `
+                <div>
+                    <div>Question: {{thisquestion}} </div>
+                </div>
+        
+        `
+        
+    })
+
+
+    Vue.component('answer',{
+        props: ['next', 'answerid'],
+        template: `
+                    <div>
+                        <input type="radio"  v-model="next" value="next" id="answerid">
+                        <label for="answerid">{{next}}</label>
+                    </div>
+        
+        `,
+        data() {
+            return{
+                answers: []
+            }
+        }
+
+
+    })
+
+   
+
+    new Vue({
+        el: '#quiz',
+        data: { 
+                
+        },
+        methods: {
+            nextQuestion(){
+                if (questionNumber <= 1) {
+                questionNumber++;
+                this.question = questions[questionNumber].question;
+                this.answers = questions[questionNumber].answers;
+                }
+            }
+        }
+    })
